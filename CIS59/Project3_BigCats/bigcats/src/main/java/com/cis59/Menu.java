659896085a5eec1f5c679971eefeae0cf62b45a2
Nicole-Rene Newcomb <112290345+Nicole-ReneNewcomb@users.noreply.github.com>
2022-10-14 02:21:47 -0700

    Find Function Implemented
    Find function implemented.

--- CIS59/Project3_BigCats/bigcats/src/main/java/com/cis59/Menu.java
+++ CIS59/Project3_BigCats/bigcats/src/main/java/com/cis59/Menu.java
@@ -26,23 +26,23 @@ public class Menu {
         printLine();
 
         /*
-            TIP:
-            In this area of the code, the additional commands need to be created and added to the menu.
-        */
+         * TIP:
+         * In this area of the code, the additional commands need to be created and
+         * added to the menu.
+         */
 
-        printCommand('c',"[C]reates a big cat");
+        printCommand('c', "[C]reates a big cat");
         printCommand('d', "[D]eletes a big cat");
         printCommand('f', "[F]inds a big cat");
-        printCommand('l',"[L]ists all big Cats");
-        printCommand('q',"[Q]uits");
-       
+        printCommand('l', "[L]ists all big Cats");
+        printCommand('q', "[Q]uits");
 
         printLine();
 
     }
 
     private static void printLine() {
-        System.out.println("----------------------------------------------------------" );
+        System.out.println("----------------------------------------------------------");
     }
 
     private static void printCommand(Character command, String desc) {
@@ -55,7 +55,7 @@ public class Menu {
         Character command = '_';
 
         String rawInput = input.nextLine();
-        
+
         if (rawInput.length() > 0) {
             rawInput = rawInput.toLowerCase();
             command = rawInput.charAt(0);
@@ -71,16 +71,17 @@ public class Menu {
         Boolean success = true;
 
         /*
-            TIP:
-            In this area of the code, the additional commands need to be created and added.
-        */
+         * TIP:
+         * In this area of the code, the additional commands need to be created and
+         * added.
+         */
 
-        switch(command) {
+        switch (command) {
 
             case 'c':
                 executeCreate(catList);
                 break;
-            
+
             case 'd':
                 executeDeleteCat(catList);
                 break;
@@ -100,7 +101,7 @@ public class Menu {
             default:
                 System.out.println("ERROR: Unknown commmand");
                 success = false;
-          }
+        }
 
         return success;
     }
@@ -109,7 +110,7 @@ public class Menu {
     public void update(LinkedList<Panthera> catList) {
 
         // update by moving all the cats
-        for (Panthera cat: catList) {
+        for (Panthera cat : catList) {
             cat.move();
         }
 
@@ -130,22 +131,22 @@ public class Menu {
     }
 
     public Panthera getNewCat(String name) {
-        
+
         /*
-            TIP:
-            In this area of the code, students need to get input from the user for the type of cat 
-            and create the correct type.
-
-            Currently, the code always create a Tiger.  But, support for Lions and Jaguars
-            also needs to be added.
-
-        */
-
+         * TIP:
+         * In this area of the code, students need to get input from the user for the
+         * type of cat
+         * and create the correct type.
+         * 
+         * Currently, the code always create a Tiger. But, support for Lions and Jaguars
+         * also needs to be added.
+         * 
+         */
 
         // request user input to determine big cat type
         Panthera result = null;
 
-        System.out.println("Is this big cat a tiger, lion, or jaguar?");
+        System.out.println("Is this big cat a tiger, lion, or jaguar? Please enter here: ");
         String userInput = input.nextLine();
 
         // convert input to lowercase
@@ -154,34 +155,29 @@ public class Menu {
         }
 
         // input validation loop to ensure entries match tiger, lion, or jaguar
-        while (!(userInput.equals("tiger") || userInput.equals("lion") || userInput.equals("jaguar")))
-        {
+        while (!(userInput.equals("tiger") || userInput.equals("lion") || userInput.equals("jaguar"))) {
             this.input = new Scanner(System.in);
-            System.out.println("Please enter one of the following options: tiger, lion, or jaguar.");
+            System.out.println("Please enter one of the following options - tiger, lion, or jaguar: ");
             userInput = input.nextLine();
             userInput = userInput.toLowerCase();
             System.out.println(userInput);
         }
 
         // decision tree to create object of user-entered big cat type
-        if (userInput.equals("tiger"))
-        {
+        if (userInput.equals("tiger")) {
             result = new Tiger(name);
         }
 
-        else if (userInput.equals("lion"))
-        {
+        else if (userInput.equals("lion")) {
             result = new Lion(name);
         }
 
-        else if (userInput.equals("jaguar"))
-        {
+        else if (userInput.equals("jaguar")) {
             result = new Jaguar(name);
         }
 
-        else
-        {
-            System.out.println("Error: Invalid input. Please be sure to enter tiger, lion, or jaguar only.");
+        else {
+            System.out.println("Error: Invalid input. Please be sure to enter tiger, lion, or jaguar only: ");
         }
 
         return result;
@@ -198,32 +194,48 @@ public class Menu {
         System.out.println();
 
         /*
-            TIP:
-            In this area of the code, students would need to add in checking if the cat name
-            already exists in order to prevent duplicates
-        */
+         * TIP:
+         * In this area of the code, students would need to add in checking if the cat
+         * name
+         * already exists in order to prevent duplicates
+         */
 
         // declare Panthera and string to hold linked list cat entries
-        Panthera cat;
+        Panthera catEntry;
         String catName;
+        boolean uniqueName;
 
-        // retrieve existing cat names from linked list via loop
-        for (Integer i = 0; i < catList.size(); i++) {
-            cat = catList.get(i);
-            catName = cat.name();
+        // loop to ensure valid, non-duplicate input for cat name
+        do {
 
-            // if cat name matches user input, prints error message
-            if (catName.equals(name)) {
+            // set unique name flag to true at start of loop
+            uniqueName = true;
 
-                System.out.println("A cat by this name already exists. Please enter a unique name.");
+            // retrieve existing cat names from linked list via loop
+            for (Integer i = 0; i < catList.size(); i++) {
+                catEntry = catList.get(i);
+                catName = catEntry.name();
+
+                // if cat name matches user input, prints error message
+                if (catName.equals(name)) {
+
+                    // sets unique name flag to false and requests new name
+                    uniqueName = false;
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    System.out.println("A cat by this name already exists. Please enter a unique name: ");
+                    name = input.nextLine();
+                }
             }
+
+        } while (uniqueName == false);
 
         Panthera cat = getNewCat(name);
         catList.add(cat);
 
     }
 
-    // list all big cats 
+    // list all big cats
     public void executeList(LinkedList<Panthera> catList) {
 
         System.out.println();
@@ -247,7 +259,34 @@ public class Menu {
 
     public void executeDeleteCat(LinkedList<Panthera> catList) {
 
+        // declare Panthera and string to hold linked list cat entries
+        Panthera catEntry;
+        String catName;
+        boolean nameExists;
 
+        // request user input to select name of cat for deletion
+        System.out.println();
+        System.out.print("Enter a name for the big cat to create: ");
+        String name = input.nextLine();
+        System.out.println();
+       
+        // retrieve existing cat names from linked list via loop
+        for (Integer i = 0; i < catList.size(); i++) {
+            catEntry = catList.get(i);
+            catName = catEntry.name();
+       
+            // if cat name matches user input, requests deletion confirmation
+            if (catName.equals(name)) {
+                           
+                // sets unique name flag to false and requests new name
+                uniqueName = false;
+                input = new Scanner(System.in);
+                System.out.println();
+                System.out.println("A cat by this name already exists. Please enter a unique name.");
+                name = input.nextLine();
+            }
+        }
+       
     }
 
     public void executeFindCat(LinkedList<Panthera> catList) {
@@ -276,9 +315,8 @@ public class Menu {
     }
 
     /*
-        TIP:
-        Additional methods and functionality need to be added to this class.
-    */
-
+     * TIP:
+     * Additional methods and functionality need to be added to this class.
+     */
 
 }
