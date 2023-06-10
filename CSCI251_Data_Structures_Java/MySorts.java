98fd13cab48e3145fff155af71e3cc12fa81a90a
Nicole-Rene Newcomb <112290345+Nicole-Rene-Newcomb@users.noreply.github.com>
2023-06-09 17:32:19 -0700

    Comments to Explain Sort Methods
    Added comments to explain which options call which methods and the sort type.

--- CSCI251_Data_Structures_Java/MySorts.java
+++ CSCI251_Data_Structures_Java/MySorts.java
@@ -1,24 +1,28 @@
 /* Nicole-Rene Newcomb's Project 1 for CSCI251, Summer 2023:
  * MySorts.java includes the code to perform four sorts - 
- * insertion sort, selection sort, quicksort, and merge sort.
- * This file is called from CSCI251ProjOne.java after the user
- * selects the desired sort from the menu.
+ * insertion (insert) sort, selection (select) sort, quick sort, 
+ * and merge sort. This file is called from CSCI251ProjOne.java 
+ * after the user selects the desired sort from the menu.
  */
 
 public class MySorts {
 
+    // Called when user selects insertion sort (option 3)
     public static void insertSort(int[] arr) {
 
     }
 
+    // Called when user selects selection sort (option 2)
     public static void selectSort (int[] arr_) {
 
     }
 
+    // Called when user selects quick sort (option 4)
     public static void quickSort(int[] arr) {
 
     }
 
+    // Called when user selects merge sort (option 5)
     public static void mergeSort(int[] arr) {
 
     }
