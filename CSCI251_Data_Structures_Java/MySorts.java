5bdd09e926ef7608c3f8794f1c1165ccc37d9833
Nicole-Rene Newcomb <112290345+Nicole-Rene-Newcomb@users.noreply.github.com>
2023-06-09 17:11:45 -0700

    Created Outline of Public Methods
    Created outline for public methods.

---
+++ CSCI251_Data_Structures_Java/MySorts.java
@@ -0,0 +1,18 @@
+public class Mysorts {
+
+    public static void insertsort(int[] arr) {
+
+    }
+
+    public static void selectSort (int[] arr_) {
+
+    }
+
+    public static void quickSort(int[] arr) {
+
+    }
+
+    public static void mergeSort(int[] arr) {
+        
+    }
+}
